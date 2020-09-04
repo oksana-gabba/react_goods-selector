08003ce0fa619e9a6f23c1464ae5e43d7e27ef84
Gabbasova Oksana <oksana.gabba@gmail.com>
2020-09-04 20:20:18 +0300

    Updates

---
+++ precache-manifest.cb447ffcbe75152466f3b0555b7eee18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b14da9059fa5d4f40cdcaa891f2bc67e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7465d03a840d4294cc16",
+    "url": "./static/css/main.ce14a850.chunk.css"
+  },
+  {
+    "revision": "368e64162689e816031d",
+    "url": "./static/js/2.0aa0400f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.0aa0400f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7465d03a840d4294cc16",
+    "url": "./static/js/main.79a025f0.chunk.js"
+  },
+  {
+    "revision": "f861d31e3669d4c8fef0",
+    "url": "./static/js/runtime-main.c970f6dd.js"
+  }
+]);
